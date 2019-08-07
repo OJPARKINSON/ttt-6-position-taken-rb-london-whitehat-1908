9e7daa33aad589a93f7583aa6daacf359f007d5a
Oliver Parkinson <olliepar01@gmail.com>
2019-08-07 14:32:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
-def position_taken?(board, input)
-  return input
-  if board[input] == " " then
+def position_taken?(board, index)
+  if board[index] == " " then
     return false
   else 
     return true
